5e338f1a9826e4a54b2e0afa07459b4dd5b5e2e3
Michiel de Jong <mbdejong@mozilla.com>
2015-09-19 19:33:28 +0200

    LazyLoader for SyncEngine

---
+++ apps/sync/js/app.js
@@ -0,0 +1,68 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use strict';
+
+/* global
+  LazyLoader,
+  SyncEngine
+*/
+
+/* exported
+  App,
+  DataAdapters
+*/
+
+var DataAdapters = {
+  // To be filled by js/adapters/*.js
+};
+
+var App = Object.freeze((() => {
+  const loadMainScripts = () => {
+    if (typeof SyncEngine !== 'undefined') {
+      return Promise.resolve();
+    }
+    return LazyLoader.load([
+      'js/crypto/stringconversion.js',
+      'js/crypto/keyderivation.js',
+      'js/crypto/fxsyncwebcrypto.js',
+
+      'js/ext/kinto-61ad959.dev.js',
+      'js/sync-engine/syncengine.js'
+    ]);
+  };
+
+  const loadAdapterScript = (collectionName) => {
+    if (typeof DataAdapters[collectionName] !== 'undefined') {
+      return Promise.resolve();
+    }
+    return LazyLoader.load(`js/adapters/${collectionName}.js`);
+  };
+
+  return {
+    /**
+      * @param request {Object} Should contain the following fields:
+      *                         * assertion
+      *                         * keys
+      *                         * collections
+      *                         * URL (of the Syncto server)
+      */
+    handleSyncRequest(request) {
+      return loadMainScripts().then(() => {
+        return Promise.all(request.collections.map(collectionName => {
+          return loadAdapterScript(collectionName);
+        }));
+      }).then(() => {
+        var se = new SyncEngine({
+          kB: request.keys.kB,
+          URL: request.URL,
+          assertion: request.assertion,
+          adapters: DataAdapters
+        });
+
+        return se.syncNow(request.collections);
+      });
+    }
+  };
+})());
