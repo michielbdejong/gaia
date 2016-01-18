43170848def94999131b7b83b8ff3627fc1a3ba9
Michiel de Jong <mbdejong@mozilla.com>
2016-01-18 20:28:22 +0100

    Set custom end-points as per https://github.com/michielbdejong/fxa-self-hosting/issues/1#issue-124459467

--- build/config/phone/custom-prefs.js
+++ build/config/phone/custom-prefs.js
@@ -2,3 +2,4 @@
 user_pref('devtools.responsiveUI.customWidth', 320);
 user_pref('devtools.responsiveUI.customHeight', 480);
 user_pref('devtools.responsiveUI.currentPreset', 'custom');
+user_pref("identity.fxaccounts.auth.uri", "https://fxa.michielbdejong.com/v1");
